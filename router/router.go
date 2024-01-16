7c343f4a58971664f4a28529f0c3cf1ad217c86c
majorbruteforce <saswat3002@gmail.com>
2024-01-17 01:50:07 +0530

    router matchers

--- router/router.go
+++ router/router.go
@@ -10,7 +10,11 @@ import (
 func Router () *mux.Router {
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", handleHome).Methods("GET")
+	s:= r.Methods("GET").Subrouter() // Prevents writing same matchers repeatedly
+	s.HandleFunc("/", handleHome)
+	s.HandleFunc("/vars/{variable}", handleRange).Methods("GET","POST")
+	s.HandleFunc("/regex/{id:[0-9]+}", handleRange)
+	
 
 	return r;
 }
@@ -18,5 +22,11 @@ func Router () *mux.Router {
 func handleHome(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	message:= "The cake is a lie"
-	fmt.Fprintf(w, "Format strings here: %s", message)
+	fmt.Fprintf(w, "Format : %s", message)
+}
+
+func handleRange(w http.ResponseWriter, r *http.Request){
+	vars:= mux.Vars(r)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w,"",vars)
 }
